bcb4a52b38118dff0bc11a306292085f67413891
drazen41 <drazen.svec@gmail.com>
2017-11-09 15:28:54 +0100

    Baseball 100%

--- Algorithms/src/BaseballElimination.java
+++ Algorithms/src/BaseballElimination.java
@@ -1,12 +1,9 @@
-import java.util.Iterator;
-
-import javax.xml.transform.Templates;
-
 import edu.princeton.cs.algs4.Bag;
 import edu.princeton.cs.algs4.FlowEdge;
 import edu.princeton.cs.algs4.FlowNetwork;
+import edu.princeton.cs.algs4.FordFulkerson;
 import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.StdOut;
 
 public class BaseballElimination {
 	
@@ -17,8 +14,6 @@ public class BaseballElimination {
 		private int loss;
 		private int left;
 		private int[] games;
-		private Bag<FlowEdge> gameFlows;
-		private FlowNetwork flowNetwork ;
 		private Team(String name, int wins, int loss, int left, int[]games, int key) {
 			this.key = key;
 			this.games = new int[games.length];
@@ -35,17 +30,26 @@ public class BaseballElimination {
 			
 		}
 	}
-	private Bag<Team> teams;
+	private FlowNetwork flowNetwork ;
+	private Bag<String> eliminated;
+	//private Bag<Team> teams;
+	private int[][]games;
+	private Team[] teams;
+	private Team[] teamNetwork;
+	private int numberOfGames=0;
+	private int source;
+	private int target;
 	public BaseballElimination(String filename) {
 		// TODO Auto-generated constructor stub
-		teams = new Bag<Team>();
+		//teams = new Bag<Team>();
+		
 		In in = new In(filename);
 		while (!in.isEmpty()) {
 			int n = in.readInt();
-			int numberOfGames = games(n-1);
+			teams = new Team[n];
+			this.games = new int[n][n];
 			Team team = null;
-			int s= -45;
-			int t= -54;
+			
 			for (int i = 0; i < n; i++) {
 				String name = in.readString();
 				int wins = in.readInt();
@@ -58,45 +62,31 @@ public class BaseballElimination {
 				team.loss = loss;
 				team.left = left;
 				team.name = name;
-				team.gameFlows = new Bag<FlowEdge>();
-				
-				team.flowNetwork = new FlowNetwork(numberOfGames + n + 1);
-//				for (int j = 0; j < n; j++) {
-//					int capacity = in.readInt();
+				for (int j = 0; j < n; j++) {
+					if (j > i) {
+						numberOfGames++;
+					}
+					int capacity = in.readInt();
 //					if (i == j) {
 //						games[j] = capacity;
 //						continue;
 //					}
-//					FlowEdge flowEdge = new FlowEdge(i,j,capacity);
 //					games[j] = capacity;
-//					team.gameFlows.add(flowEdge);
-//				}
-				for (int j = 0; j < n; j++) {
-					int capacity = in.readInt();
-					if (i == j || j < i) {
-						games[j] = capacity;
-						continue;
-					}
-					FlowEdge gameFlow = new FlowEdge(i,j,capacity);
-					team.flowNetwork.addEdge(gameFlow );
+					this.games[i][j]=capacity;
 				}
-				
-				teams.add(team);
-				team.flowNetwork.toString();
+
+				team.games = games;
+				//teams.add(team);
+				teams[i] = team;
 			}
 			
 			
 			
 		}
 	}
-	private int games(int n) {
-		if (n == 1) {
-			return 1;
-		} 
-		return n * games(n-1);
-	}
+	
 	public int numberOfTeams() {  // number of teams
-		return teams.size();
+		return teams.length;
 	}
 	public Iterable<String> teams() {                                // all teams
 		Bag<String> names = new Bag<String>();
@@ -106,14 +96,7 @@ public class BaseballElimination {
 		return names;
 		
 	}
-	private boolean teamsContains(String team) {
-		for (String tim : teams()) {
-			if (tim.equals(team)) {
-				return true;
-			}
-		}
-		return false;
-	}
+	
 	private Team team(String team) {
 		if (team == null) {
 			return null;
@@ -153,38 +136,96 @@ public class BaseballElimination {
 		if (tim1 == null || tim2 == null) {
 			throw new IllegalArgumentException();
 		}
-		
-		return 0;
+		int remaining = this.games[tim1.key][tim2.key];
+		return remaining;
 	}
 	public boolean isEliminated(String team) {              // is given team eliminated?
 		Team tim = team(team);
 		if (tim == null) {
 			throw new IllegalArgumentException();
 		}
+		eliminated = new Bag<String>();
 		// trivial
+		int i = 0;
+		teamNetwork = new Team[teams.length-1];
 		for (Team team2 : teams) {
 			if (team2.name.equals(tim.name )) {
 				continue;
 			}
 			if (team2.wins > tim.wins + tim.left ) {
+				eliminated.add(team2.name);
 				return true;
 			}
+			teamNetwork[i] = team2;
+			i++;
 		}
 		// nontrivial
-		
-		for (Team team2 : teams) {
-			if (team2.name.equals(tim.name )) {
+		createFlowNetworkFor(tim);
+		FordFulkerson fordFulkerson = new FordFulkerson(flowNetwork , source, target);
+//		StdOut.println(team + " maxflow: " + fordFulkerson.value());
+		for (int j = 0; j < teams.length; j++) {
+			if (tim.key == j) {
 				continue;
 			}
-			
+			if (fordFulkerson.inCut(j)) {
+				eliminated.add(teams[j].name);
+				
+			}
 		}
-		
+        if (!eliminated.isEmpty()) {
+			return true;
+		}  
 		return false;
 	}
+	private void createFlowNetworkFor(Team tim) {
+		int n = teams.length;
+		int numberOfVertices = numberOfGames + 3;
+		int s= numberOfGames+1;
+		int t= numberOfGames+2;
+		source = s;
+		target = t;
+		//int j = n-1;
+//		StdOut.println("Flow network for: " + tim.name);
+		flowNetwork = new FlowNetwork(numberOfVertices);
+		int teamInd = 0;
+		for (int i = 0; i < teams.length; i++) {
+			if (teams[i].name.equals(tim.name)) {
+				teamInd = i;
+			}
+		}
+		for (int i = 0; i < games.length; i++) {
+			if (i == teamInd) {
+				continue;
+			}
+			Team team = teams[i];
+			int teamCapacity = tim.wins + tim.left - team.wins;
+			FlowEdge targetFlow = new FlowEdge(i,t,teamCapacity);
+			flowNetwork.addEdge(targetFlow);
+			for (int j = 0; j < games.length; j++) {
+				if (j == teamInd || i == j || j < i) {
+					continue;
+				}
+				int to1 = i;
+				int to2 = j;
+				FlowEdge gameFlow1 = new FlowEdge(n,to1,Double.POSITIVE_INFINITY);
+				FlowEdge gameFlow2 = new FlowEdge(n,to2,Double.POSITIVE_INFINITY);
+				FlowEdge sourceFlow = new FlowEdge(s,n,games[i][j]);
+				flowNetwork.addEdge(gameFlow1);
+				flowNetwork.addEdge(gameFlow2);
+				flowNetwork.addEdge(sourceFlow);				
+				n++;
+			}
+		}
+//		StdOut.println(flowNetwork.toString());
+	}
+
 	public Iterable<String> certificateOfElimination(String team) {  // subset R of teams that eliminates given team; null if not eliminated
 		Team tim = team(team);
 		if (tim == null) {
 			throw new IllegalArgumentException();
+		}
+		if (isEliminated(team)) {
+			return eliminated;
 		}
 		return null;
 	}
--- Algorithms/src/BaseballTest.java
+++ Algorithms/src/BaseballTest.java
@@ -5,10 +5,12 @@ public class BaseballTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub				
 		BaseballElimination division = new BaseballElimination(args[0]);
-		StdOut.println(division.wins("Montreal"));
-		StdOut.println(division.losses("Montreal"));
-		StdOut.println(division.remaining("Montreal"));
-		StdOut.println(division.isEliminated("Montreal"));
+//		StdOut.println(division.wins("Montreal"));
+//		StdOut.println(division.losses("Detroit"));
+//		StdOut.println(division.remaining("Detroit"));
+//		StdOut.println(division.against("Detroit", "Toronto"));
+	//	StdOut.println(division.isEliminated("Detroit"));
+		
 	    for (String team : division.teams()) {
 	        if (division.isEliminated(team)) {
 	            StdOut.print(team + " is eliminated by the subset R = { ");
